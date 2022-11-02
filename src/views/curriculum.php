9cb032dc2b13275668e003571a76b2c32e3a5a0a
Paulo Vareiro <58229468+paulovareiro29@users.noreply.github.com>
2022-11-02 13:35:32 +0000

    added section visiblity on curriculum

--- src/views/curriculum.php
+++ src/views/curriculum.php
@@ -33,103 +33,119 @@
     <div class="profile">
       <div id="curriculum">
         <div class="profile-header">
-          <div class="profile-picture">
-            <img
-              src=<?= $curriculum['avatar']?>
-              alt="<?= $curriculum['person_name']?>'s avatar"
-            />
-          </div>
+          <?php if(!empty($curriculum['avatar'])):?>
+            <div class="profile-picture">
+              <img
+                src=<?= $curriculum['avatar']?>
+                alt="<?= $curriculum['person_name']?>'s avatar"
+              />
+            </div>
+          <?php endif;?>
           
-          <div class="profile-header-info">
-            <h2>Hello, I am</h2>
-            <h1><?= $curriculum['person_name']?></h1>
-            <h4><?= $curriculum['role']?></h4>
-          </div>
+          <?php if(!empty($curriculum['person_name']) && !empty($curriculum['role'])):?>
+            <div class="profile-header-info">
+              <?php if(!empty($curriculum['person_name'])):?>
+                <h2>Hello, I am</h2>
+              <?php endif;?>
+              <h1><?= $curriculum['person_name']?></h1>
+              <h4><?= $curriculum['role']?></h4>
+            </div>
+          <?php endif;?>  
         </div>
         <div class="profile-body">
           <!-- LEFT COLUMN -->
           <div class="col">
-            <section class="info-section">
-              <h2><?= $curriculum['info_header']?></h2>
-              
-              <?php foreach($curriculum['info'] as $item):?>
-                <div>
-                  <?php if(empty($item['href'])):?>
-                    <i class="fa fa-2x <?=$item['icon']?>"></i>
-                    <?=$item['content']?>
-                  <?php else:?>
-                    <a href="<?= $item['href'] ?>">
+            <?php if(count($curriculum['info']) > 0):?>
+              <section class="info-section">
+                <h2><?= $curriculum['info_header']?></h2>
+                
+                <?php foreach($curriculum['info'] as $item):?>
+                  <div>
+                    <?php if(empty($item['href'])):?>
                       <i class="fa fa-2x <?=$item['icon']?>"></i>
                       <?=$item['content']?>
-                    </a>
-                  <?php endif;?>
-                </div>
-              <?php endforeach;?>
-            </section>
+                    <?php else:?>
+                      <a href="<?= $item['href'] ?>">
+                        <i class="fa fa-2x <?=$item['icon']?>"></i>
+                        <?=$item['content']?>
+                      </a>
+                    <?php endif;?>
+                  </div>
+                <?php endforeach;?>
+              </section>
+            <?php endif;?>
 
-            <section class="skills-section">
-              <h2><?= $curriculum['skills_header']?></h2>
-              
-              <?php foreach($curriculum['skills'] as $item):?>
-                <div>
-                  <p><?=$item['content']?></p>
-                  <ul>
-                    <?php for($x = 0; $x < $item['rating']; $x++):?>
-                      <li><i class="fa fa-circle"></i></li>
-                    <?php endfor;?>
+            <?php if(count($curriculum['skills']) > 0):?>
+              <section class="skills-section">
+                <h2><?= $curriculum['skills_header']?></h2>
+                
+                <?php foreach($curriculum['skills'] as $item):?>
+                  <div>
+                    <p><?=$item['content']?></p>
+                    <ul>
+                      <?php for($x = 0; $x < $item['rating']; $x++):?>
+                        <li><i class="fa fa-circle"></i></li>
+                      <?php endfor;?>
 
-                    <?php for($x = 0; $x < 5-$item['rating']; $x++):?>
-                      <li><i class="fa-regular fa-circle"></i></li>
-                    <?php endfor;?>
-                  </ul>
-                </div>
-              <?php endforeach;?>
-            </section>
+                      <?php for($x = 0; $x < 5-$item['rating']; $x++):?>
+                        <li><i class="fa-regular fa-circle"></i></li>
+                      <?php endfor;?>
+                    </ul>
+                  </div>
+                <?php endforeach;?>
+              </section>
+            <?php endif;?>
 
-            <section class="education-section">
-              <h2><?= $curriculum['education_header']?></h2>
-              
-              <?php foreach($curriculum['education'] as $item):?>
-                <div class="education">
-                  <p class="education-date">
-                    <i class="fa fa-calendar"></i>
-                    <?= $item['start_date']?> - <?=$item['end_date']?>
-                  </p>
-                  <h4><?= $item['school']?></h4>
-                  <p><?= $item['course']?></p>
-                  <p><small><?= $item['location']?></small></p>
-                </div>
-              <?php endforeach;?>
-            </section>
+            <?php if(count($curriculum['education']) > 0):?>
+              <section class="education-section">
+                <h2><?= $curriculum['education_header']?></h2>
+                
+                <?php foreach($curriculum['education'] as $item):?>
+                  <div class="education">
+                    <p class="education-date">
+                      <i class="fa fa-calendar"></i>
+                      <?= $item['start_date']?> - <?=$item['end_date']?>
+                    </p>
+                    <h4><?= $item['school']?></h4>
+                    <p><?= $item['course']?></p>
+                    <p><small><?= $item['location']?></small></p>
+                  </div>
+                <?php endforeach;?>
+              </section>
+            <?php endif;?>
           </div>
 
           <!-- RIGHT COLUMN -->
           <div class="col">
-            <section class="profile-section">
-              <h2><?= $curriculum['profile_header']?></h2>
-              <p>
-                <?= $curriculum['summary']?>
-              </p>
-            </section>
+            <?php if(!empty($curriculum['summary'])):?>
+              <section class="profile-section">
+                <h2><?= $curriculum['profile_header']?></h2>
+                <p>
+                  <?= $curriculum['summary']?>
+                </p>
+              </section>
+            <?php endif;?>
 
-            <section class="experience-section">
-              <h2><?= $curriculum['experience_header']?></h2>
-              
-              <?php foreach($curriculum['experience'] as $item):?>
-                <div class="experience">
-                <div class="experience-title">
-                  <h4><?= $item['company']?></h4>
-                  <p class="experience-date">
-                    <i class="fa fa-calendar"></i>
-                    <?= $item['start_date']?> - <?= $item['end_date']?>
-                  </p>
+            <?php if(count($curriculum['experience']) > 0):?>
+              <section class="experience-section">
+                <h2><?= $curriculum['experience_header']?></h2>
+                
+                <?php foreach($curriculum['experience'] as $item):?>
+                  <div class="experience">
+                  <div class="experience-title">
+                    <h4><?= $item['company']?></h4>
+                    <p class="experience-date">
+                      <i class="fa fa-calendar"></i>
+                      <?= $item['start_date']?> - <?= $item['end_date']?>
+                    </p>
+                  </div>
+                  <p><?= $item['role']?></p>
+                  <div><?= $item['summary']?></div>
                 </div>
-                <p><?= $item['role']?></p>
-                <div><?= $item['summary']?></div>
-              </div>
-              <?php endforeach;?>
+                <?php endforeach;?>
 
-            </section>
+              </section>
+            <?php endif;?>
           </div>
         </div>
       </div>
