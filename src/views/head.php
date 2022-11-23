7c89a7de82c931158922958f52286454ce1d2399
Paulo Vareiro <58229468+paulovareiro29@users.noreply.github.com>
2022-11-23 13:02:45 +0000

    added favicon

--- src/views/head.php
+++ src/views/head.php
@@ -1,6 +1,13 @@
 <meta charset="UTF-8" />
 <meta http-equiv="X-UA-Compatible" content="IE=edge" />
 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+<!-- Favicon -->
+<link rel="apple-touch-icon" sizes="180x180" href="/<?= $_ENV['SRC_DIR']?>/assets/images/favicon/apple-touch-icon.png">
+<link rel="icon" type="image/png" sizes="32x32" href="/<?= $_ENV['SRC_DIR']?>/assets/images/favicon/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="16x16" href="/<?= $_ENV['SRC_DIR']?>/assets/images/favicon/favicon-16x16.png">
+<link rel="manifest" href="/<?= $_ENV['SRC_DIR']?>/assets/images/favicon/site.webmanifest">
+
 <link rel="preconnect" href="https://fonts.googleapis.com" />
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
 <link
