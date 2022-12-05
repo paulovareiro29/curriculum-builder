53431bf54e1b8e05bc596fcd14989c3e9efc5b6d
Paulo Vareiro <58229468+paulovareiro29@users.noreply.github.com>
2022-12-05 19:08:59 +0000

    added landing buttons for loggenIn users

--- src/views/landing.php
+++ src/views/landing.php
@@ -18,8 +18,12 @@
             <p>Creating a curriculum vitae can be overwhelming, especially when you have to do it all by yourself. You can also check other people curriculums or make yours public for everyone to see!</p>
             <p>This platform was built as an <strong>university project.</strong></p>
             <div class="cta">
-                <a href="/<?= $_ENV["BASE_DIR"] ?>/login" class="btn btn-primary">Sign In</a>
-                <a href="/<?= $_ENV["BASE_DIR"] ?>/register" class="btn btn-outline">Create an account</a>
+                <?php if (AuthController::isLoggedIn()) : ?>
+                    <a href="/<?= $_ENV["BASE_DIR"] ?>/backoffice" class="btn btn-primary">Dashboard</a>
+                <?php else : ?>
+                    <a href="/<?= $_ENV["BASE_DIR"] ?>/login" class="btn btn-primary">Sign In</a>
+                    <a href="/<?= $_ENV["BASE_DIR"] ?>/register" class="btn btn-outline">Create an account</a>
+                <?php endif; ?>
             </div>
         </div>
 
