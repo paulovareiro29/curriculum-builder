bcacf53884d144eb23c26c285588b5defbf8da45
Paulo Vareiro <58229468+paulovareiro29@users.noreply.github.com>
2022-12-10 16:21:29 +0000

    added guide to edit curriculum

--- src/views/editCurriculum.php
+++ src/views/editCurriculum.php
@@ -45,6 +45,134 @@ foreach ($curriculum['managers'] as $manager) {
 <?php require_once(__ROOT__ . "/layout/body.php") ?>
 <?php require_once(__ROOT__ . "/layout/navbar.php") ?>
 
+<div class="modal guide-modal" id="guide-general">
+  <div class="modal-background"></div>
+  <div class="modal-wrapper">
+    <h4 class="modal-title">
+      How does it work?
+      <i class="fa-solid fa-xmark modal-close"></i>
+    </h4>
+    <div class="modal-body">
+      <div>
+        <p><i class="fa-solid fa-house"></i> <strong>General</strong> is the information about your curriculum.</p>
+        <p>1) You can change the curriculums <strong>name</strong> and <strong>description</strong> that is shown.</p>
+        <p>2) You can also change if the you want to <strong>showcase</strong> your curriculum or not.</p>
+      </div>
+      <button type="button" class="btn btn-md" onclick="closeModal('guide-general')">GOT IT</button>
+    </div>
+  </div>
+</div>
+
+<div class="modal guide-modal" id="guide-profile">
+  <div class="modal-background"></div>
+  <div class="modal-wrapper">
+    <h4 class="modal-title">
+      How does it work?
+      <i class="fa-solid fa-xmark modal-close"></i>
+    </h4>
+    <div class="modal-body">
+      <div>
+        <p><i class="fa-solid fa-user"></i> <strong>Profile</strong> is the information about you!</p>
+        <p>1) You can change your <strong>picture</strong>, <strong>name</strong>, <strong>role</strong> and <strong>summary</strong></p>
+        <p>2) Click on <strong>Profile</strong> header to edit it!</p>
+      </div>
+      <button type="button" class="btn btn-md" onclick="closeModal('guide-profile')">GOT IT</button>
+    </div>
+  </div>
+</div>
+
+<div class="modal guide-modal" id="guide-info">
+  <div class="modal-background"></div>
+  <div class="modal-wrapper">
+    <h4 class="modal-title">
+      How does it work?
+      <i class="fa-solid fa-xmark modal-close"></i>
+    </h4>
+    <div class="modal-body">
+      <div>
+        <p><i class="fa-solid fa-circle-info"></i> <strong>Info</strong> is where you place your links and social media pages!</p>
+        <p>1) Change the <strong>content</strong>, <strong>href</strong> and <strong>icon</strong> of each information. </p>
+        <p>2) Href will be placed directly on the link. Use it like the <strong>HTML</strong> attribute. It can also be empty.</p>
+        <p>3) Click on <strong>Info</strong> header to edit it!</p>
+      </div>
+      <button type="button" class="btn btn-md" onclick="closeModal('guide-info')">GOT IT</button>
+    </div>
+  </div>
+</div>
+
+<div class="modal guide-modal" id="guide-skills">
+  <div class="modal-background"></div>
+  <div class="modal-wrapper">
+    <h4 class="modal-title">
+      How does it work?
+      <i class="fa-solid fa-xmark modal-close"></i>
+    </h4>
+    <div class="modal-body">
+      <div>
+        <p><i class="fa-solid fa-star"></i> <strong>Skills</strong> is where you showcase all of your most important skills and its rating.</p>
+        <p>1) Change the <strong>content</strong> and <strong>rating</strong></p>
+        <p>2) <strong>Rating</strong> is a range from 1 to 5</p>
+        <p>3) Click on <strong>Skills</strong> header to edit it!</p>
+      </div>
+      <button type="button" class="btn btn-md" onclick="closeModal('guide-skills')">GOT IT</button>
+    </div>
+  </div>
+</div>
+
+<div class="modal guide-modal" id="guide-education">
+  <div class="modal-background"></div>
+  <div class="modal-wrapper">
+    <h4 class="modal-title">
+      How does it work?
+      <i class="fa-solid fa-xmark modal-close"></i>
+    </h4>
+    <div class="modal-body">
+      <div>
+        <p><i class="fa-sharp fa-solid fa-graduation-cap"></i> <strong>Education</strong> is where you show where you have studied.</p>
+        <p>1) Change the <strong>start</strong> and <strong>end</strong> date, <strong>school name</strong>, <strong>course</strong> and <strong>school location</strong></p>
+        <p>2) Click on <strong>Education</strong> header to edit it!</p>
+      </div>
+      <button type="button" class="btn btn-md" onclick="closeModal('guide-education')">GOT IT</button>
+    </div>
+  </div>
+</div>
+
+<div class="modal guide-modal" id="guide-experience">
+  <div class="modal-background"></div>
+  <div class="modal-wrapper">
+    <h4 class="modal-title">
+      How does it work?
+      <i class="fa-solid fa-xmark modal-close"></i>
+    </h4>
+    <div class="modal-body">
+      <div>
+        <p><i class="fa-solid fa-building"></i> <strong>Experience</strong> is where you show all of your previous experience.</p>
+        <p>1) Change the <strong>start</strong> and <strong>end</strong> date, <strong>company name</strong>, <strong>role</strong> and <strong>summary</strong></p>
+        <p>2) <strong>Summary</strong> can be HTML tags!</p>
+        <p>3) Click on <strong>Experience</strong> header to edit it!</p>
+      </div>
+      <button type="button" class="btn btn-md" onclick="closeModal('guide-experience')">GOT IT</button>
+    </div>
+  </div>
+</div>
+
+<div class="modal guide-modal" id="guide-managers">
+  <div class="modal-background"></div>
+  <div class="modal-wrapper">
+    <h4 class="modal-title">
+      How does it work?
+      <i class="fa-solid fa-xmark modal-close"></i>
+    </h4>
+    <div class="modal-body">
+      <div>
+        <p><i class="fa-solid fa-user-group"></i> <strong>Managers</strong> is where you control your curriculum managers.</p>
+        <p>Each manager has <strong>access</strong> to the messages of the curriculums that they manage. They can mark each message and <strong>read</strong> / <strong>unread</strong></p>
+      </div>
+      <button type="button" class="btn btn-md" onclick="closeModal('guide-managers')">GOT IT</button>
+    </div>
+  </div>
+</div>
+
 <div class="container">
   <div id="badge-edit-success" class="alert alert-success d-none">Curriculum has been saved successfuly!</div>
   <div id="badge-edit-error" class="alert alert-danger d-none">An error occured while trying to save.</div>
@@ -81,7 +209,10 @@ foreach ($curriculum['managers'] as $manager) {
   <form id="edit-form" data-curriculum="<?= $curriculum['id'] ?>" class="col" enctype="multipart/form-data">
     <div id="main-content" class="form-row">
       <div id="general">
-        <h1>General</h1>
+        <div class="header">
+          <h1>General</h1>
+          <button type="button" onclick="showModal('guide-general')" class="btn btn-circle-md guide-btn">?</button>
+        </div>
         <div class="form-row">
           <div class="form-group">
             <label for="name">Name</label>
@@ -103,7 +234,10 @@ foreach ($curriculum['managers'] as $manager) {
       </div>
 
       <div id="profile">
-        <input type="text" name="profile_header" class="editable-header" data-field="profile_header" value="<?= $curriculum['profile_header'] ?>" />
+        <div class="header">
+          <input type="text" name="profile_header" class="editable-header" data-field="profile_header" value="<?= $curriculum['profile_header'] ?>" />
+          <button type="button" onclick="showModal('guide-profile')" class="btn btn-circle-md guide-btn">?</button>
+        </div>
 
         <div class="profile-header">
           <div>
@@ -148,7 +282,10 @@ foreach ($curriculum['managers'] as $manager) {
       </div>
 
       <div id="info">
-        <input type="text" name="info_header" class="editable-header" data-field="info_header" value="<?= $curriculum['info_header'] ?>" />
+        <div class="header">
+          <input type="text" name="info_header" class="editable-header" data-field="info_header" value="<?= $curriculum['info_header'] ?>" />
+          <button type="button" onclick="showModal('guide-info')" class="btn btn-circle-md guide-btn">?</button>
+        </div>
 
         <button class="btn btn-primary" type="button" id="add-info">Add new</button>
 
@@ -181,7 +318,10 @@ foreach ($curriculum['managers'] as $manager) {
       </div>
 
       <div id="skills">
-        <input type="text" name="skills_header" class="editable-header" data-field="skills_header" value="<?= $curriculum['skills_header'] ?>" />
+        <div class="header">
+          <input type="text" name="skills_header" class="editable-header" data-field="skills_header" value="<?= $curriculum['skills_header'] ?>" />
+          <button type="button" onclick="showModal('guide-skills')" class="btn btn-circle-md guide-btn">?</button>
+        </div>
 
         <button class="btn btn-primary" type="button" id="add-skill">Add new</button>
 
@@ -205,7 +345,10 @@ foreach ($curriculum['managers'] as $manager) {
       </div>
 
       <div id="education">
-        <input type="text" name="education_header" class="editable-header" data-field="education_header" value="<?= $curriculum['education_header'] ?>" />
+        <div class="header">
+          <input type="text" name="education_header" class="editable-header" data-field="education_header" value="<?= $curriculum['education_header'] ?>" />
+          <button type="button" onclick="showModal('guide-education')" class="btn btn-circle-md guide-btn">?</button>
+        </div>
 
         <button class="btn btn-primary" type="button" id="add-education">Add new</button>
 
@@ -247,7 +390,10 @@ foreach ($curriculum['managers'] as $manager) {
       </div>
 
       <div id="experience">
-        <input type="text" name="experience_header" class="editable-header" data-field="experience_header" value="<?= $curriculum['experience_header'] ?>" />
+        <div class="header">
+          <input type="text" name="experience_header" class="editable-header" data-field="experience_header" value="<?= $curriculum['experience_header'] ?>" />
+          <button type="button" onclick="showModal('guide-experience')" class="btn btn-circle-md guide-btn">?</button>
+        </div>
 
         <button class="btn btn-primary" type="button" id="add-experience">Add new</button>
 
@@ -289,7 +435,10 @@ foreach ($curriculum['managers'] as $manager) {
       </div>
 
       <div id="managers">
-        <h1>Managers</h1>
+        <div class="header">
+          <h1>Managers</h1>
+          <button type="button" onclick="showModal('guide-managers')" class="btn btn-circle-md guide-btn">?</button>
+        </div>
 
         <button class="btn btn-primary" type="button" id="add-manager">Add new</button>
 
